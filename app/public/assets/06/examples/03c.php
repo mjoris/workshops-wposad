002d8096376f75d97ae81e6f006acf2f73b15c5c
Joris Maervoet <joris.maervoet@odisee.be>
2022-10-13 17:26:18 +0200

    actualizing slides on databases

--- app/public/assets/06/examples/03c.php
+++ app/public/assets/06/examples/03c.php
@@ -17,7 +17,6 @@
 	// Make Connection
 	try {
 		$db = new PDO('mysql:host=' . DB_HOST .';dbname=' . DB_NAME_FF . ';charset=utf8mb4', DB_USER, DB_PASS);
-		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	} catch (PDOException $e) {
 		showDbError('connect', $e->getMessage());
 	}
