94a6e88838e0446320554ffb1a1d2728283acb58
Joris Maervoet <joris.maervoet@odisee.be>
2024-10-03 22:02:29 +0200

    update slides and examples on PHP basics

--- app/public/assets/03/examples/redirect.php
+++ app/public/assets/03/examples/redirect.php
@@ -1,8 +1,8 @@
 <?php
 
-	header('Location: http://www.ikdoeict.be/');
-	exit(); // do not forget to exit after redirect
+header('Location: https://www.odisee.be');
+exit(); // do not forget to exit after redirect
 
-	echo 'This line will be ingored';
+echo 'This line will be ingored';
 
 //EOF
--- app/public/assets/03/examples/weight.php
+++ app/public/assets/03/examples/weight.php
@@ -1,21 +1,26 @@
 <?php
 
-class Weight {
+class Weight
+{
 
-	private float $weight;
-	private string $unit;
+    private float $weight;
+    private string $unit;
 
-	public function __construct(float $weight, string $unit) {
-		$this->weight = $weight;
-		$this->unit = $unit;
-	}
+    public function __construct(float $weight, string $unit)
+    {
+        $this->weight = $weight;
+        $this->unit = $unit;
+    }
 
-	public function __toString() : string {
-		return $this->weight . ' ' . $this->unit;
-	}
+    #[Override] // I know this method is overriding a parent method. If that would ever change, please let me know
+    public function __toString(): string
+    {
+        return $this->weight . ' ' . $this->unit;
+    }
 }
 
 $myWeight = new Weight(82.5, 'kg');
-?><pre><?php
-echo($myWeight);
-?></pre>
+?>
+<pre><?php
+    echo($myWeight);
+    ?></pre>
